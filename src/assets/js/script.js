ce7c4779828c7a12ab8778e468cd15e9a0dd01fc
Abdul Rehman <mehars.6925@gmail.com>
2020-04-30 04:15:32 +0500

    added functionality to toggle sidebar

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -9,6 +9,14 @@
         burger.classList.toggle('is-active')
     })
 
-    
+    // Sidebar
+    let sidebarToggler = document.querySelector('.sidebar-toggler')
+    let sidebarParentColumn = document.querySelector('.menu.side-menu').parentElement
+
+    sidebarToggler.addEventListener('click', () => {
+        console.log(sidebarParentColumn)
+        sidebarParentColumn.classList.toggle('is-visible')
+    })
+
 
 })()
